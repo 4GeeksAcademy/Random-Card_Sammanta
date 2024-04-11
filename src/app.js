4fbc291bcccd99661ac3f1589794f94c807c8190
Sammantha73 <Samanthafeijoom@gmail.com>
2024-04-11 17:14:59 +0000

    CAMBIOS

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = randomCards();
+const button = document.getElementById("button");
+button.addEventListener("click", randomCards);
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function randomCards() {
+  let symbols = ["&spades;", "&clubsuit;", "&hearts;", "&diams;"];
+  let randomSymbols = Math.floor(Math.random() * symbols.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let randomNumbers = Math.floor(Math.random() * numbers.length);
+
+  document.getElementById("card").className = symbols[randomSymbols];
+
+  document.getElementById("thesymbol").innerHTML = symbols[randomSymbols];
+  document.getElementById("mynumber").innerHTML = numbers[randomNumbers];
+  document.getElementById("thesymbol2").innerHTML = symbols[randomSymbols];
+}
+setTimeout(randomCards, 3000);
